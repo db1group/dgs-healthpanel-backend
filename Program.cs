7fb42f83750f68e84ff2d4c86df783c6657fc4be
Alexandro Hervis <alexandro.hervis@db1.com.br>
2023-04-19 14:48:21 -0300

    remove https redirect

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+// app.UseHttpsRedirection();
+// app.UseAuthentication();
+// app.UseAuthorization();
 app.MapControllers();
 app.Run();
