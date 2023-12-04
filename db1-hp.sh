2a83f1960bacc1b0993fc1969427d249012183d7
Levi Alves DB1 <levi.alves@db1.com.br>
2023-12-04 15:23:15 -0300

    Add params clear app db

--- db1-hp.sh
+++ db1-hp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 
 check_existing_docker() {
     if command -v docker >/dev/null || command -v podman >/dev/null && command -v docker-compose >/dev/null; then
@@ -89,17 +89,54 @@ check_existing_browser() {
 }
 
 main() {
-    check_existing_docker
+    HC_PATH=$(pwd)
 
-    echo "Digite sua senha de intranet para copiar o arquivo Dump PostgreSQL"
-    scp $USER@10.200.10.16:/tmp/backup_all_databases.sql /tmp/
-    docker run --name HealthPanelDevPostgres -e POSTGRES_USER=healthpanel -e POSTGRES_PASSWORD=healthpanel -e POSTGRES_DB=healthpanelprocess -d postgres:15.3
-    sleep 5
-    docker exec -i HealthPanelDevPostgres psql -U healthpanel -d healthpanelprocess < /tmp/backup_all_databases.sql
-    dotnet run appsettings.Development.json &
-    sleep 5
+    echo "DB1 Global Software - Health Process"
 
-    check_existing_browser
+    COMPONENT=$HC_PATH/$1
+
+    if [ "$#" -lt 1 ]; then
+        echo ""
+        echo "Por favor, forneça o parâmetro!"
+        echo ""
+        echo "1. db1@dgs$ ./db1-hp db  ---> Only DB"
+        echo "2. db1@dgs$ ./db1-hp app ---> Only App + DB"
+        echo "3. db1@dgs$ ./db1-hp clear ---> Clear the project"
+        echo ""
+    else
+        if [ "$1" = "db" ]; then
+            check_existing_docker
+            echo "Digite sua senha de intranet para copiar o arquivo Dump PostgreSQL"
+            scp $USER@10.200.10.16:/tmp/backup_all_databases.sql /tmp/
+            docker run --name HealthPanelDevPostgres -e POSTGRES_USER=healthpanel -e POSTGRES_PASSWORD=healthpanel -e POSTGRES_DB=healthpanelprocess -d postgres:15.3
+            sleep 5
+            docker exec -i HealthPanelDevPostgres psql -U healthpanel -d healthpanelprocess < /tmp/backup_all_databases.sql
+        elif [ "$1" = "app" ]; then
+            check_existing_docker
+
+            echo "Digite sua senha de intranet para copiar o arquivo Dump PostgreSQL"
+            scp $USER@10.200.10.16:/tmp/backup_all_databases.sql /tmp/
+            docker run --name HealthPanelDevPostgres -e POSTGRES_USER=healthpanel -e POSTGRES_PASSWORD=healthpanel -e POSTGRES_DB=healthpanelprocess -d postgres:15.3
+            sleep 5
+            docker exec -i HealthPanelDevPostgres psql -U healthpanel -d healthpanelprocess < /tmp/backup_all_databases.sql
+            dotnet run appsettings.Development.json &
+            sleep 5
+
+            check_existing_browser
+        elif [ "$1" = "clear" ]; then
+            echo "Cleaning..."
+            docker container stop HealthPanelDevPostgres 2>/dev/null
+            docker container prune -f 2>/dev/null
+            docker image rm -f postgres:15.3 2>/dev/null
+            HCPID=`lsof -i :7101 | awk '{print $2}'` 2>/dev/null
+            kill -9 $HCPID 2>/dev/null
+            sleep 5
+            echo "Done !"
+        else
+            echo
+            echo "Parâmetro inválido. Use 'db' ou 'app'."
+        fi
+    fi
 }
 
-main
+main "$@"
